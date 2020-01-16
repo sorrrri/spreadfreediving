1af46b2e94eb1fcdc9bfa647f5835e95f5937149
soridive <soridive@naver.com>
2020-01-17 00:35:39 +0900

    test

--- sources/js/sori.js
+++ sources/js/sori.js
@@ -1,22 +1,14 @@
 $(document).ready(function () {
     $(window).scroll(function () {
 
-        const currentScroll = $(this).scrollTop();
+        const currentScroll2 = $(this).scrollTop();
         const ypos = window.pageYOffset;
         
-        if (currentScroll > 100) {
+        if (currentScroll2 > 100) {
             $("header").addClass("active");
         }
-        if (currentScroll < 100) {
+        if (currentScroll2 < 100) {
             $("header").removeClass("active");
-        }
-
-
-
-        //parallax
-        if (currentScroll > $("section.about").offset().top) {
-            $(".parallax-text").css("transform", "translateX(" + ypos * -.5 + "px)");
-            $(".parallax-text-02").css("transform", "translateX(" + ypos * .5 + "px)");
         }
     })
 })
