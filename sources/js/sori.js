e62b423fe8d6a57d4c4f5b29eb8f68d599c9fc68
coralcat <you@example.com>
2020-01-23 20:09:55 +0900

    layout

--- sources/js/sori.js
+++ sources/js/sori.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
 
-        const currentScroll2 = $(this).scrollTop();
-        const ypos = window.pageYOffset;
-        
-        if (currentScroll2 > 100) {
-            $("header").addClass("active");
-        }
-        if (currentScroll2 < 100) {
-            $("header").removeClass("active");
-        }
+    $("a, button, canvas, .slick-slider").on("mouseenter", function () {
+        $(".segment").addClass("straighten");
+
     })
+
+    $("a, button, canvas, .slick-slider").on("mouseleave", function () {
+
+        $(".segment").removeClass("straighten");
+
+    })
+
 })
